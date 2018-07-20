9b2e79f4f0835972dcbf20b56b4608c055cdf430
刘涛 <tao_liu@sui.com>
2018-07-20 10:17:31 +0800

    feat(目录结构): 更新目录结构，抽离calf-ui到单独文件夹

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,17 +1,24 @@
+// https://eslint.org/docs/user-guide/configuring
+
 module.exports = {
-  'root': true,
-  'env': {
-    'browser': true,
-    'mocha': true,
-    'es6':true,
-    'node': true
+  root: true,
+  parserOptions: {
+    parser: 'babel-eslint'
   },
-  'extends': 'standard',
-  'parserOptions': {
-    'sourceType': 'module',
-    'ecmaVersion': 2017
+  env: {
+    browser: true
   },
-  'rules': {
+  extends: [
+    // https://github.com/vuejs/eslint-plugin-vue#priority-a-essential-error-prevention
+    // consider switching to `plugin:vue/strongly-recommended` or `plugin:vue/recommended` for stricter rules.
+    'plugin:vue/essential',
+    // https://github.com/standard/standard/blob/master/docs/RULES-en.md
+    'standard'
+  ],
+  // required to lint *.vue files
+  plugins: ['vue'],
+  // add your custom rules here
+  rules: {
     'space-before-function-paren': 0,
     // allow async-await
     'generator-star-spacing': 'off',
--- README.md
+++ README.md
@@ -1,29 +1,22 @@
-### kn-cnpm
+# calf-ui
 
-[cnpm](http://cnpm-registry.cardniu.work) 私有库代码
+> A mobile ui lib implement by Vue
 
-#### 文件夹说明
+## 构建步骤
+
+```bash
+# 安装依赖
+npm install
+
+# 运行example
+npm run dev
+
+# 运行文档
+npm run doc
+
+# 编译组件库，生成lib文件夹
+npm run build
+
+# 编译demo项目
+npm run doc
 ```
-    - @cardniu 所有模块都放置@cardniu作用域下
-```
-
-#### 项目约定
-
-- 1、每个package以小写英文单词+中划线组成，不能用驼峰模式
-- 2、项目已经整合eslint, 写代码时候需要开启，可以在外层目录执行 npm run lint
-- 3、每新加一个package需要添加单元测试，请参考demo
-- 4、每个package目录规定如下：
-
-```
-    - package-name
-        - test #测试文件目录 【必须】
-        - dist #输出的打包文件目录【可选】
-        - src  #源代码目录【可选】
-        - index.js #入口文件 
-        - package.json 【必须】
-        - readMe.md 【必须】
-```
-
-- 5、提交代码前需要在项目第一层执行：npm run lint
-
-
