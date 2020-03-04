4352e3366b31789ba8fbc3fa5b8563d39cc858b3
liutao <947809647@qq.com>
2020-03-04 20:04:49 +0800

    feat: 升级webpack版本&抽离组件css为单独文件

--- build-webpack4/webpack.dev.conf.js
+++ build-webpack4/webpack.dev.conf.js
@@ -1,41 +1,45 @@
-'use strict';
+'use strict'
 // const utils = require('./utils');
-const config = require('../config');
-const webpack = require('webpack');
-const merge = require('webpack-merge');
-const path = require('path');
-const VueLoaderPlugin = require('vue-loader/lib/plugin');
+// const config = require('../config')
+// const webpack = require('webpack')
+// const merge = require('webpack-merge')
+const path = require('path')
+const VueLoaderPlugin = require('vue-loader/lib/plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+// const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 
-const HOST = process.env.HOST;
-const PORT = process.env.PORT && Number(process.env.PORT);
-const isProd = process.env.NODE_ENV === 'production';
+// const HOST = process.env.HOST
+// const PORT = process.env.PORT && Number(process.env.PORT)
+const isProd = process.env.NODE_ENV === 'production'
 
 function resolve(dir) {
-  return path.join(__dirname, '..', dir);
+  return path.join(__dirname, '..', dir)
 }
 // console.log(config);
 // console.log(`${HOST}--${PORT}`);
 console.log('--isProd--', isProd)
 
 var entry = {
-  app: './example/main.js'
-};
+  // app: resolve('./build-webpack4/webpack-test/main.js'),
+  app: resolve('./example/main.js')
+  // app: resolve('./src/index.js')
+}
 
 var output = {
   publicPath: '', // js引用路径或者CDN地址
   path: resolve('dist'), // 打包文件的输出目录
   filename: 'bundle.js'
-};
+}
 
-var jsExclude = file => /node_modules/.test(file) && !/\.vue\.js/.test(file);
+// 过滤node_modules部分且防止导入的vue单文件组件里的script标签被排除
+var jsExclude = file => /node_modules/.test(file) && !/\.vue\.js/.test(file)
 
 module.exports = {
   entry: entry,
   output: output,
   mode: 'development',
-  devtool: 'source-map',
+  devtool: 'cheap-eval-source-map',
   resolve: {
     extensions: ['.js', '.vue', '.json'],
     alias: {
@@ -61,15 +65,63 @@ module.exports = {
         exclude: jsExclude
       },
       {
-        test: /\.css$/,
-        use: [isProd ? 'vue-style-loader' : MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader']
+        test: /\.(post)?css$/, // 匹配css或postcss格式的文件
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              importLoaders: 1,
+              alias: {
+                '~': resolve('./src')
+              }
+            }
+          },
+          'postcss-loader'
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          'vue-style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+              localIdentName: '[local]_[hash:base64:8]'
+            }
+          },
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              name: '[name]-[hash:5].min.[ext]',
+              limit: 20000,
+              publicPath: 'static/',
+              outputPath: 'static/'
+            }
+          }
+        ]
       }
     ]
   },
   plugins: [
-    new MiniCssExtractPlugin({
-      filename: 'style.css'
+    // new MiniCssExtractPlugin({
+    //   filename: 'style.css'
+    // }),
+    new HtmlWebpackPlugin({
+      filename: 'index.html',
+      template: resolve('./example/index.html'),
+      chunks: ['app'],
+      minify: {
+        collapseWhitespace: true
+      }
     }),
     new VueLoaderPlugin()
   ]
-};
+}
